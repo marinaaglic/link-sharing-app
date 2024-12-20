ba7fbf1110b4fa72307e1e5b00cd0c63a8135939
marinaaglic <marinaaglic11@gmail.com>
2024-12-20 17:23:22 +0100

    created a registration component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Login from "./components/login/Login";
+// import Login from "./components/login/Login";
+import Registration from "./components/registration/Registration";
 
 function App() {
   return (
     <>
-      <Login />
+      <Registration />
     </>
   );
 }

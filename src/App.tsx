2e19b5708b252ef6b445ede12e69a817998ea375
marinaaglic <marinaaglic11@gmail.com>
2024-12-24 21:34:53 +0100

    implemented responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import LoginPage from "./pages/login/LoginPage";
-import RegistrationPage from "./pages/registration/RegistrationPage";
+// import RegistrationPage from "./pages/registration/RegistrationPage";
 
 function App() {
   return (

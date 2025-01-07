7ce14114d504eb143acad64d115a68b020736831
marinaaglic <marinaaglic11@gmail.com>
2025-01-07 20:32:57 +0100

    fix validation messages

--- src/utils/schema/index.ts
+++ src/utils/schema/index.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().min(1, "Can't be empty"),
+  email: z
+    .string()
+    .min(1, "Can't be empty")
+    .email("This is not a valid e-mail"),
   password: z.string().min(8, "Please check again"),
 });

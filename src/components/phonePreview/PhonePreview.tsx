4cda188c528aa74c3b8eb2a261b95c865f8de8dd
marinaaglic <marinaaglic11@gmail.com>
2025-01-25 13:31:52 +0100

    styling fix

--- src/components/phonePreview/PhonePreview.tsx
+++ src/components/phonePreview/PhonePreview.tsx
@@ -1,6 +1,8 @@
+import styles from "./phonePreview.module.css";
+
 export default function PhonePreview() {
   return (
-    <div>
+    <div className={styles.phoneWrapper}>
       <div>Mobile preview</div>
     </div>
   );

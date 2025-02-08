ce84598e978599854d1fa459c386d7128ed36f1d
marinaaglic <marinaaglic11@gmail.com>
2025-02-08 14:35:44 +0100

    import fix

--- src/pages/links/LinksPage.tsx
+++ src/pages/links/LinksPage.tsx
@@ -1,6 +1,6 @@
 import NavBar from "../../components/navbar/NavBar";
 import CustomizeLinks from "../../components/customizeLinks/CustomizeLinks";
-import PhonePreview from "../../components/phonePreview/phonePreview";
+import PhonePreview from "../../components/phonePreview/PhonePreview";
 import styles from "./linksPage.module.css";
 
 export default function LinksPage() {

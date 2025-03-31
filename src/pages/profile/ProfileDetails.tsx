bc5809b8df49ff2d091e14e1e1af52999d016d8c
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-03-31 18:09:10 +0200

    added phone preview and navbar to profile details page

--- src/pages/profile/ProfileDetails.tsx
+++ src/pages/profile/ProfileDetails.tsx
@@ -1,5 +1,17 @@
+import styles from "./ProfileDetails.module.css";
+import NavBar from "../../components/navbar/NavBar";
+import PhonePreview from "../../components/phonePreview/PhonePreview";
+
 export default function ProfileDetailsPage() {
   return (
-    <div>ProfileDetails</div>
+    <div className={styles.pageContainer}>
+      <NavBar />
+      <div className={styles.pageContent}>
+        <PhonePreview />
+      </div>
+      <div className={styles.profileDetails}>
+        
+      </div>
+    </div>
   )
 }

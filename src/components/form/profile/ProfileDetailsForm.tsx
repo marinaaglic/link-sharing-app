40cf1719a48d568120cf722ec971cdf70ba53aed
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-04-01 18:31:35 +0200

    [WIP] form for adding profile details added

---
+++ src/components/form/profile/ProfileDetailsForm.tsx
@@ -0,0 +1,33 @@
+import styles from "./ProfileDetailsForm.module.css";
+import Input from "../../reusable/input/Input";
+
+export default function ProfileDetailsForm() {
+  return (
+    <div className={styles.customizeWrapper}>
+        <div>
+            <h2>Profile Details</h2>
+            <p>Add your details to add personal touch to your profile.</p>
+        </div>
+        <form>
+            <Input 
+            label="First name"
+            id="firstName"
+            type="text"
+            placeholder="e.g. John"
+            />
+            <Input 
+            label="Last name"
+            id="lastName"
+            type="text"
+            placeholder="e.g. Appleseed"
+            />
+            <Input 
+            label="Email"
+            id="email"
+            type="email"
+            placeholder="e.g. email@example.com"
+            />
+        </form>
+    </div>
+  )
+}
--- src/pages/profile/ProfileDetails.tsx
+++ src/pages/profile/ProfileDetails.tsx
@@ -1,17 +1,20 @@
 import styles from "./ProfileDetails.module.css";
 import NavBar from "../../components/navbar/NavBar";
 import PhonePreview from "../../components/phonePreview/PhonePreview";
+import ProfileDetailsForm from "../../components/form/profile/ProfileDetailsForm";
 
 export default function ProfileDetailsPage() {
   return (
     <div className={styles.pageContainer}>
-      <NavBar />
-      <div className={styles.pageContent}>
+    <NavBar />
+    <div className={styles.pageContent}>
+      <div className={styles.phonePreviewContainer}>
         <PhonePreview />
       </div>
       <div className={styles.profileDetails}>
-        
+        <ProfileDetailsForm />
       </div>
     </div>
+  </div>
   )
 }

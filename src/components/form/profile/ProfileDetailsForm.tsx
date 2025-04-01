64f075b46118c70bfdec124365c3f2c35d072e50
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-04-01 18:53:34 +0200

    [WIP] styling added to profile details form

--- src/components/form/profile/ProfileDetailsForm.tsx
+++ src/components/form/profile/ProfileDetailsForm.tsx
@@ -9,24 +9,27 @@ export default function ProfileDetailsForm() {
             <p>Add your details to add personal touch to your profile.</p>
         </div>
         <form>
-            <Input 
-            label="First name"
-            id="firstName"
-            type="text"
-            placeholder="e.g. John"
-            />
-            <Input 
-            label="Last name"
-            id="lastName"
-            type="text"
-            placeholder="e.g. Appleseed"
-            />
-            <Input 
-            label="Email"
-            id="email"
-            type="email"
-            placeholder="e.g. email@example.com"
-            />
+            <div className={styles.inputElements}>
+                <Input 
+                label="First name"
+                id="firstName"
+                type="text"
+                placeholder="e.g. John"
+                />
+                <Input 
+                label="Last name"
+                id="lastName"
+                type="text"
+                placeholder="e.g. Appleseed"
+                />
+                <Input 
+                label="Email"
+                id="email"
+                type="email"
+                placeholder="e.g. email@example.com"
+                />
+            </div>
+           
         </form>
     </div>
   )

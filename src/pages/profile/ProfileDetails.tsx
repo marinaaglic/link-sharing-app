33ec15ca4bfa09c50ac02d5e38252e737a5ea708
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-04-11 18:40:59 +0200

    implemented SharedLayout to ProfileDetails

--- src/pages/profile/ProfileDetails.tsx
+++ src/pages/profile/ProfileDetails.tsx
@@ -1,20 +1,12 @@
-import styles from "./ProfileDetails.module.css";
-import NavBar from "../../components/navbar/NavBar";
-import PhonePreview from "../../components/phonePreview/PhonePreview";
 import ProfileDetailsForm from "../../components/form/profile/ProfileDetailsForm";
+import { SharedLayout } from "../../components/layout/SharedLayout";
 
 export default function ProfileDetailsPage() {
   return (
-    <div className={styles.pageContainer}>
-      <NavBar />
-      <div className={styles.pageContent}>
-        <div className={styles.phonePreviewContainer}>
-          <PhonePreview />
-        </div>
-        <div className={styles.profileDetails}>
-          <ProfileDetailsForm />
-        </div>
-      </div>
+    <div>
+      <SharedLayout>
+        <ProfileDetailsForm />
+      </SharedLayout>
     </div>
   );
 }

3ec102c4b752baeee28e8a7d3cc63feedfa553e6
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-04-17 16:27:45 +0200

    removed inputContainer div

--- src/components/reusable/input/Input.tsx
+++ src/components/reusable/input/Input.tsx
@@ -8,18 +8,16 @@ function Input(
   ref: React.ForwardedRef<HTMLInputElement>
 ) {
   return (
-    <div className={styles.inputContainer}>
-      <div className={styles.inputWrapper}>
-        <input
-          id={id}
-          name={id}
-          ref={ref}
-          {...rest}
-          type={type}
-          className={clsx(styles.input, { [styles.inputError]: error })}
-        />
-        {error && <span className={styles.errorText}>{error}</span>}
-      </div>
+    <div className={styles.inputWrapper}>
+      <input
+        id={id}
+        name={id}
+        ref={ref}
+        {...rest}
+        type={type}
+        className={clsx(styles.input, { [styles.inputError]: error })}
+      />
+      {error && <span className={styles.errorText}>{error}</span>}
     </div>
   );
 }

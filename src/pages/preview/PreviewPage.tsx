9a743992fccb5edc5cff86f2ae77119dad25f13b
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-05-19 21:42:51 +0200

    added green background on half of page

--- src/pages/preview/PreviewPage.tsx
+++ src/pages/preview/PreviewPage.tsx
@@ -1,8 +1,9 @@
 import PreviewNavBar from "../../components/navbar/PreviewNavBar";
+import styles from "./PreviewPage.module.css";
 
 export default function PreviewPage() {
   return (
-    <div>
+    <div className={styles.pageContainer}>
       <PreviewNavBar />
     </div>
   );

1646f1075fafa438cbc2d3b09482ff9ab0a052f3
Marina Aglić Čuvić <marinaaglic11@gmail.com>
2025-05-20 18:03:01 +0200

    profile details card implemented

--- src/components/profileDetails/ProfileDetailsCard.tsx
+++ src/components/profileDetails/ProfileDetailsCard.tsx
@@ -1,5 +1,14 @@
 import styles from "./ProfileDetails.module.css";
 
 export default function ProfileDetailsCard() {
-  return <div>ProfileDetailsCard</div>;
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.profilePicture}></div>
+      <div className={styles.userInfo}>
+        <h2>Name</h2>
+        <span>E-mail</span>
+      </div>
+      <div className={styles.addedLinks}></div>
+    </div>
+  );
 }
--- src/pages/preview/PreviewPage.tsx
+++ src/pages/preview/PreviewPage.tsx
@@ -1,10 +1,12 @@
 import PreviewNavBar from "../../components/navbar/PreviewNavBar";
+import ProfileDetailsCard from "../../components/profileDetails/ProfileDetailsCard";
 import styles from "./PreviewPage.module.css";
 
 export default function PreviewPage() {
   return (
     <div className={styles.pageContainer}>
       <PreviewNavBar />
+      <ProfileDetailsCard />
     </div>
   );
 }
